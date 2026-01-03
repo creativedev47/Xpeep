d7bcbcd6121431b66ab51ae02f571deaf946733b
codeaj001 <codeaj001@gmail.com>
2026-01-03 16:13:17 +0100

    xportal update

--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,5 +1,5 @@
 // Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
-export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+export const walletConnectV2ProjectId = '497d0e57eec052dbb1af0e086f398d96';
 export const apiTimeout = 6000;
 export const transactionSize = 10;
 export const nativeAuth = true;
